68da067b83885f025778f0f61e36bce6174491ea
shuvo18103107 <18103107@iubat.edu>
2021-02-13 02:10:23 +0600

    adding images and applying logic when there is no transaction

--- lib/widget/transaction_list.dart
+++ lib/widget/transaction_list.dart
@@ -1,3 +1,5 @@
+// import 'package:universal_html/html.dart';
+
 import '../model/transaction.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
@@ -10,40 +12,57 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
         height: 300,
-        child: ListView.builder(
-          itemBuilder: (ctx, index) {
-            return Card(
-                child: Row(
-              children: [
-                Container(
-                  padding: EdgeInsets.all(10),
-                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                  decoration: BoxDecoration(
-                      border: Border.all(color: Colors.purple, width: 2)),
-                  child: Text(
-                    '\$${transactionList[index].amount.toStringAsFixed(2)}',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      color: Colors.purple,
-                    ),
+        child: transactionList.isEmpty
+            ? Column(
+                children: [
+                  Text(
+                    'No transaction added yet!',
+                    style: Theme.of(context).textTheme.headline6,
                   ),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(transactionList[index].title,
-                        style: Theme.of(context).textTheme.headline6),
-                    Text(
-                      DateFormat.yMMMd().format(transactionList[index].date),
-                      style: TextStyle(color: Colors.grey),
-                    ),
-                  ],
-                )
-              ],
-            ));
-          },
-          itemCount: transactionList.length,
-        ));
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                      height: 200,
+                      child: Image.asset('assets/images/waiting.png'))
+                ],
+              )
+            : ListView.builder(
+                itemBuilder: (ctx, index) {
+                  return Card(
+                      child: Row(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(10),
+                        margin:
+                            EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.purple, width: 2)),
+                        child: Text(
+                          '\$${transactionList[index].amount.toStringAsFixed(2)}',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.purple,
+                          ),
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(transactionList[index].title,
+                              style: Theme.of(context).textTheme.headline6),
+                          Text(
+                            DateFormat.yMMMd()
+                                .format(transactionList[index].date),
+                            style: TextStyle(color: Colors.grey),
+                          ),
+                        ],
+                      )
+                    ],
+                  ));
+                },
+                itemCount: transactionList.length,
+              ));
   }
 }
