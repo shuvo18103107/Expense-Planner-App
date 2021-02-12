1b9c3534660ef584bfa924ac744206db044acffc
shuvo18103107 <18103107@iubat.edu>
2021-02-13 01:12:29 +0600

    💡 custom fonts and working with texttheme

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,24 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'personal expense',
-        theme:
-            ThemeData(primarySwatch: Colors.purple, accentColor: Colors.amber),
         debugShowCheckedModeBanner: false,
+        title: 'personal expense',
+        theme: ThemeData(
+          textTheme: ThemeData.light().textTheme.copyWith(
+              headline6: TextStyle(
+                  fontFamily: 'OpenSans',
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold)),
+          primarySwatch: Colors.purple,
+          accentColor: Colors.amber,
+          // fontFamily: 'Quicksand',
+          appBarTheme: AppBarTheme(
+              textTheme: ThemeData.light().textTheme.copyWith(
+                  headline6: TextStyle(
+                      fontFamily: 'OpenSans',
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold))),
+        ),
         home: Home());
   }
 }
--- lib/widget/transaction_list.dart
+++ lib/widget/transaction_list.dart
@@ -32,13 +32,8 @@ class TransactionList extends StatelessWidget {
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text(
-                      transactionList[index].title,
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
+                    Text(transactionList[index].title,
+                        style: Theme.of(context).textTheme.headline6),
                     Text(
                       DateFormat.yMMMd().format(transactionList[index].date),
                       style: TextStyle(color: Colors.grey),
