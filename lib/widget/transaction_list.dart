3aee3d0045cccbed5bc07a6f90ac4f8eaabbdd7f
shuvo18103107 <18103107@iubat.edu>
2021-02-14 03:34:39 +0600

    🚀 wrap up left the ios adjustmant and some other context ,business logic all done

--- lib/widget/transaction_list.dart
+++ lib/widget/transaction_list.dart
@@ -12,7 +12,7 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        height: 300,
+        height: 450,
         child: transactionList.isEmpty
             ? Column(
                 children: [
