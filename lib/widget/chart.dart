99b4b832ac07878f9fb23dde6c722d0bb5c30630
shuvo18103107 <18103107@iubat.edu>
2021-02-13 21:03:32 +0600

    📝 creating bars for our chart

--- lib/widget/chart.dart
+++ lib/widget/chart.dart
@@ -8,6 +8,7 @@ class Chart extends StatelessWidget {
 
   List<Map<String, Object>> get grouptransactionvalue {
     return List.generate(7, (index) {
+      // last 7 days dekhabe currebt date theke suru kore
       final weekday = DateTime.now().subtract(Duration(days: index));
       double totalamount = 0.0;
       for (int i = 0; i < recentTransaction.length; i++) {
@@ -17,19 +18,25 @@ class Chart extends StatelessWidget {
           totalamount = totalamount + recentTransaction[i].amount;
         }
       }
-      print(weekday);
+      print(DateFormat.E().format(weekday));
       print(totalamount);
-      return {'day': DateFormat.E(weekday), 'amount': totalamount};
+      return {
+        'day': DateFormat.E().format(weekday).substring(0, 1),
+        'amount': totalamount
+      };
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    print(grouptransactionvalue);
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),
       child: Row(
-        children: [],
+        children: grouptransactionvalue.map((data) {
+          return Text('${data['day']}:${data['amount']}');
+        }).toList(),
       ),
     );
   }
---
+++ lib/widget/chart_bar.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class Barchart extends StatelessWidget {
+  final String weekday;
+  final double spendingAmount;
+  final double spendingPcOfTotal;
+
+  Barchart(this.weekday, this.spendingAmount, this.spendingPcOfTotal);
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text('\$${spendingAmount.toStringAsFixed(0)}'),
+        SizedBox(
+          height: 4,
+        ),
+        Container(
+          height: 60,
+          width: 10,
+          child: Stack(
+            children: [
+              // bottom widget
+              Container(
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey, width: 1.0),
+                    color: Color.fromRGBO(220, 220, 220, 1),
+                    borderRadius: BorderRadius.circular(10)),
+              ),
+              // now rest of the portion need blank so we use fractionallysizedbox widget
+              FractionallySizedBox(
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                      borderRadius: BorderRadius.circular(10)),
+                ),
+                heightFactor: spendingPcOfTotal,
+              )
+            ],
+          ),
+        ),
+        SizedBox(
+          height: 4,
+        ),
+        Text(weekday),
+      ],
+    );
+  }
+}
