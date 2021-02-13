ae0162504499f74ca7cd03a1e2318cebfef00ac4
shuvo18103107 <18103107@iubat.edu>
2021-02-13 22:02:20 +0600

    📄 populating the charts with values

--- lib/widget/chart.dart
+++ lib/widget/chart.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import '../model/transaction.dart';
 import 'package:intl/intl.dart';
+import './chart_bar.dart';
 
 class Chart extends StatelessWidget {
   final List<Transaction> recentTransaction;
   Chart(this.recentTransaction);
 
-  List<Map<String, Object>> get grouptransactionvalue {
+  List<Map<String, Object>> get _grouptransactionvalue {
     return List.generate(7, (index) {
       // last 7 days dekhabe currebt date theke suru kore
       final weekday = DateTime.now().subtract(Duration(days: index));
@@ -27,15 +28,31 @@ class Chart extends StatelessWidget {
     });
   }
 
+  // total amount in last 7 days
+  double get _totalSpendInweek {
+    // 1st argument starting value r 2nd e collection ba map er ekta value dore 1t argument e add korte thakbo ete collection reduce hoye ekta singel value dibe
+    return _grouptransactionvalue.fold(0.0, (sum, item) {
+      return sum + item['amount'];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    print(grouptransactionvalue);
+    print(_grouptransactionvalue);
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),
       child: Row(
-        children: grouptransactionvalue.map((data) {
-          return Text('${data['day']}:${data['amount']}');
+        children: _grouptransactionvalue.map((data) {
+          // per map er jonno ekta chart draw kori row te r sei tar value pass korci
+          // / operator use korle ki typer value pass kori indicate kore dite hbe i mean type of object
+          return Barchart(
+              data['day'],
+              data['amount'],
+              //  ekhn data server e nai tai app restart korle sob data loss hoia jai tai kono transiction thake na tai chart e totalspending  0 thakar karone 0/0  pass korar karone app crush khai tai logic dite hobe
+              _totalSpendInweek == 0.0
+                  ? 0.0
+                  : (data['amount'] as double) / _totalSpendInweek);
         }).toList(),
       ),
     );
