ca7920fa1a0f07776b7e93dcb6826e58786a59ea
shuvo18103107 <18103107@iubat.edu>
2021-02-24 22:49:47 +0600

    🐛 some changes

--- lib/widget/new-transactionList.dart
+++ lib/widget/new-transactionList.dart
@@ -10,7 +10,8 @@ class UserInput extends StatefulWidget {
 }
 
 class _UserInputState extends State<UserInput> {
-  final _titlecontroller = TextEditingController();
+  final _titlecontroller =
+      TextEditingController(); // use kore kono input field er value store korte pari
 
   final _amountcontroller = TextEditingController();
   DateTime _selectedDate;
@@ -18,24 +19,27 @@ class _UserInputState extends State<UserInput> {
   void _submitData() {
     // DateTime _selectedDate;
 
-    final enterTitle = _titlecontroller.text;
-    final enterAmount = double.parse(_amountcontroller.text);
-
+    final enterTitle =
+        _titlecontroller.text; //.text diye value ki likhlo store korlam
+    final enterAmount =
+        double.parse(_amountcontroller.text); //jehutu double typer value chai
+//validation code
     if (enterTitle.isEmpty ||
         enterAmount <= 0 ||
         _amountcontroller.text.isEmpty ||
         _selectedDate == null) {
-      return;
+      return; //mane eikhanei function execution off hoye jabe
     }
     // using widget we can acess widget class property inside the stat
 
     widget.addTrans(enterTitle, enterAmount, _selectedDate);
-    Navigator.of(context).pop();
+    // Navigator.of(context).pop();
   }
 
   void _presentDatePicker() {
     // showdate picker is future , futures are classes that allows to create object which gives us value in future
     showDatePicker(
+            // ekhane direct root context e jabe sekhane giye dekhbe kothai ei function er ref ace se jaigai show korbe jehutu root context sob position hold kore widget er
             context: context,
             firstDate: DateTime(2020, 6),
             lastDate: DateTime.now(),
@@ -75,13 +79,15 @@ class _UserInputState extends State<UserInput> {
               keyboardType: TextInputType.number,
               // onChanged: (val) => amountInput = val,
               controller: _amountcontroller,
-              onSubmitted: (_) => _submitData(),
+              onSubmitted: (_) =>
+                  _submitData(), // anonymous function use korci tai argument e _ diya vujalam value paici but i dont care to use it
             ),
             Container(
               height: 70,
               child: Row(
                 children: [
                   Expanded(
+                    // expanded use korlam jate eita choose date er space bade baki sob space dokhol kore nei
                     child: Text(
                       _selectedDate == null
                           ? 'No Date Choosen!'
